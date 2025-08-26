9408abefec94186dc54ccc1b326cf77bda5a029a
sanderson <caique.borges@al.infnet.edu.br>
2025-08-25 22:09:43 -0300

    Initial Commit

---
+++ src/main/java/com/infnet/fitcoach_saas/FitcoachSaasApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.fitcoach_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitcoachSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitcoachSaasApplication.class, args);
+	}
+
+}
