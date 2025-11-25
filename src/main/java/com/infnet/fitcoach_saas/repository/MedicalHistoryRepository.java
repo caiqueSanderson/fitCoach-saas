2a89d7c4cfcbc074ad30f060c0a7e3c6eaa49672
sanderson <caique.borges@al.infnet.edu.br>
2025-11-24 22:19:53 -0300

    Refactor

--- src/main/java/com/infnet/fitcoach_saas/repository/MedicalHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infnet.fitcoach_saas.repository;
-
-import com.infnet.fitcoach_saas.models.MedicalHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
-}
--- src/main/java/com/infnet/fitcoach_saas/repository/TraineeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infnet.fitcoach_saas.repository;
-
-import com.infnet.fitcoach_saas.models.Trainee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TraineeRepository extends JpaRepository<Trainee, Long> {
-}
